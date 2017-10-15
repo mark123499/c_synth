03f3592eaf9db5625d40f47cb1499479690c8c3f
mark123499 <mark123499@hoge.com>
2017-10-15 21:20:48 +0900

    Add code to generate sine wave.

--- c_synth.h
+++ c_synth.h
@@ -1,26 +1,8 @@
 #ifndef C_SYNTH_H
 #define C_SYNTH_H
 
-typedef struct 
-{
-	char riff_str[STR_LEN];
-	long filesize;
-	char wave_str[STR_LEN];
-} waveHdr;
-
-typedef struct
-{
-	char fmt_str[STR_LEN];
-	long fmtsize;
-	short fmtcode;
-	short channels;
-	long samplingrate;
-	long byte_per_sec;
-	short block_size;
-	short bits;
-} waveFmtChunk;
-
 #define STR_LEN 4
+
 #define RIFF_STR "RIFF"
 #define WAVE_STR "WAVE"
 #define FMT_STR  "fmt "
@@ -30,5 +12,30 @@ typedef struct
 #define DEF_CHANNEL_NUM 2
 #define DEF_SAMPLING_RATE 44100
 #define DEF_BIT_DEPTH 16
+
+typedef struct 
+{
+	char riff_str[STR_LEN];
+	long filesize;
+	char wave_str[STR_LEN];
+} waveHdr;
+
+typedef struct
+{
+	char  fmt_str[STR_LEN];
+	long  chunk_size;
+	short fmt_code;
+	short channels;
+	long  sampling_rate;
+	long  byte_per_sec;
+	short block_size;
+	short bit_depth;
+} waveFmtChunk;
+
+typedef struct
+{
+	char data_str[STR_LEN];
+	long chunk_size;
+} waveDataChunk;
 
 #endif
